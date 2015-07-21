a75235fb5b1d997df13fca5a71755f90764734a9
davecarter <dave74@gmail.com>
2015-07-21 23:17:52 +0200

    DONE! finally.. var multiples should be declared outside the loop

--- src/iterators.js
+++ src/iterators.js
@@ -1,11 +1,15 @@
 const isMultipleOfFive = (n) => n % 5 === 0;
 
 const getMultiplesOfFive = function(a, b) {
+  var multiples = [];
   for (var i = a; i <= b; i++) {
+    
     if(isMultipleOfFive(i)){
-      console.log('retorna sólo números múltiples de 5');
+      var result = multiples.push(i);
+      console.log('retorna sólo números múltiples de 5 como el ' + multiples);
     }
   };
+  return multiples;
 }
 
 export {isMultipleOfFive, getMultiplesOfFive};
