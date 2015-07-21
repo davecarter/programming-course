d37a83224c4b9e317dfce8d8d22ec75594cb9c10
davecarter <dave74@gmail.com>
2015-07-21 21:34:58 +0200

    retorna sólo cuando un número es múltiple de 5

--- src/iterators.js
+++ src/iterators.js
@@ -2,7 +2,9 @@ const isMultipleOfFive = (n) => n % 5 === 0;
 
 const getMultiplesOfFive = function(a, b) {
   for (var i = a; i <= b; i++) {
-    console.log('retorna rango de números entre a y b');
+    if(i % 5 === 0){
+      console.log('retorna sólo números múltiples de 5');
+    }
   };
 }
 
