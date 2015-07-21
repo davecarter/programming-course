8bfd47e86e03ad26f8e6063b55c841818dbdbb4b
davecarter <dave74@gmail.com>
2015-07-21 21:37:20 +0200

    if condition refactored to use previous function

--- src/iterators.js
+++ src/iterators.js
@@ -2,7 +2,7 @@ const isMultipleOfFive = (n) => n % 5 === 0;
 
 const getMultiplesOfFive = function(a, b) {
   for (var i = a; i <= b; i++) {
-    if(i % 5 === 0){
+    if(isMultipleOfFive(i)){
       console.log('retorna sólo números múltiples de 5');
     }
   };
